a2b46108dee3bc3c1dee4f95c24fd3d906973def
Taylor C. Richberger <taywee@gmx.com>
2022-08-05 09:00:43 -0600

    rework iter_forever for the new recv

--- recv.lua
+++ recv.lua
@@ -3,21 +3,27 @@ local sard <const> = require 'sard'
 -- Tools around receiving data.
 local module <const> = {}
 
+local function iter_forever(state)
+  local value, time
+  if state.timeout then
+    value, time = sard.recv(state.key, state.timeout)
+  else
+    value, time = sard.recv(state.key)
+  end
+  return time, value
+end
+
 -- Return an infinite iterator that recvs forever and gives timestamps and
 -- values.
 -- The order is like this because valid values may be nil, which would stop
 -- iteration.
-function module.forever(key, interval)
+function module.forever(key, timeout)
   assert(key)
-  return function()
-    local value, time
-    if interval then
-      value, time = sard.recv(key, interval)
-    else
-      value, time = sard.recv(key)
-    end
-    return time, value
-  end
+  local state <const> = {
+    key = key,
+    timeout = timeout,
+  }
+  return iter_forever, state
 end
 
 return module
